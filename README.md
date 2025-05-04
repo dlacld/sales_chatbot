4c7c8891e0111de851894837c740211a3bd3feb9
dlacld <1301816722@qq.com>
2025-05-04 11:44:04 +0800

    V1 Update

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# sales_chatbot
+# Multi Scenario-Chatbot 
+
+
+
+
+## 介绍
+
+Multi Scenario-Chatbot 是一个使用 AI LLM结合 RAG 技术 实现基于不同场景下的 AI  机器人 智能问答系统.
+
+这个工具使用了大型语言模型 (LLMs) 。  它是用 Python 构建的，并且具有灵活、模块化和面向对象的设计。
+
+
+
+## 特性
+
+- [x] 基于Langchain 大模型中间件开发
+- [x] 图形化场景进行机器人聊天
+- [x] 涉及房产，英语培训，手机及汽车 4个领域的知识问答
+
+
+## 开始使用
+
+### 环境准备
+
+1.克隆仓库 `git clone `
+
+2.需要 Python 3.10 或更高版本
+
+3.默认使用OpenAI LLM gpt-3.5-turbo 进行调用
+
+### 使用示例
+
+您可以通过指定配置文件或提供命令行参数来使用 OpenAI-Translator 工具。
+
+命令行直接运行：
+
+```bash
+python sales_chatbot.py
+```
+
+
+
+## 运行页面案例
+
+![Clipboard_Screenshot_1746330129](/tmp/iOALog/.new_Clipboard/Clipboard_Screenshot_1746330129.png)
+
+
+
+## 许可证
+
+该项目采用 GPL-3.0 许可证。有关详细信息，请查看 [LICENSE](LICENSE) 文件。
+
+### 参考
+
+此项目参考https://github.com/DjangoPeng/openai-quickstart
+
+
+
+
+
--- sales_chatbot.py
+++ sales_chatbot.py
@@ -3,57 +3,124 @@ import random
 import time
 
 from typing import List
+from enum import Enum, unique, auto
 
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
 
+# 是否激活 AI 回复问题
+ENABLE_CHAT = False
 
-def initialize_sales_bot(vector_store_dir: str="real_estates_sale"):
-    db = FAISS.load_local(vector_store_dir, OpenAIEmbeddings())
+
+@unique
+class Scene_enum(Enum):
+    房产 = "real_estate"
+    手机知识 = "iphone"
+    英语培训 = "english_training"
+    汽车知识 = "car"
+
+
+def initialize_sales_bot(vector_store_dir: str = "real_estate_sales"):
+    db = FAISS.load_local(vector_store_dir, OpenAIEmbeddings(),allow_dangerous_deserialization=True)
     llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
-    
-    global SALES_BOT    
-    SALES_BOT = RetrievalQA.from_chain_type(llm,
-                                           retriever=db.as_retriever(search_type="similarity_score_threshold",
-                                                                     search_kwargs={"score_threshold": 0.8}))
+
+    global SALES_BOT
+    SALES_BOT = RetrievalQA.from_chain_type(
+        llm,
+        retriever=db.as_retriever(
+            search_type="similarity_score_threshold",
+            search_kwargs={"score_threshold": 0.6},
+        ),
+    )
     # 返回向量数据库的检索结果
     SALES_BOT.return_source_documents = True
 
     return SALES_BOT
 
-def sales_chat(message, history):
-    print(f"[message]{message}")
-    print(f"[history]{history}")
-    # TODO: 从命令行参数中获取
-    enable_chat = True
 
-    ans = SALES_BOT({"query": message})
-    # 如果检索出结果，或者开了大模型聊天模式
-    # 返回 RetrievalQA combine_documents_chain 整合的结果
-    if ans["source_documents"] or enable_chat:
+def add_text(history, text):
+    # print(f"add_text-[history]{history}")
+    # print(f"add_text-[text]{text}")
+
+    history = history + [(text, None)]
+    return history, gr.update(value="", interactive=False)
+
+
+def bot(history, text):
+    # print(f"bot-[history]{history}")
+    # print(f"bot-[text]{text}")
+    query = history[-1][0]
+
+    ans = SALES_BOT({"query": query})
+
+    response = "这个问题我要问问领导"
+    if ans["source_documents"] or ENABLE_CHAT:
         print(f"[result]{ans['result']}")
         print(f"[source_documents]{ans['source_documents']}")
-        return ans["result"]
-    # 否则输出套路话术
-    else:
-        return "这个问题我要问问领导"
-    
+        response = ans["result"]
 
-def launch_gradio():
-    demo = gr.ChatInterface(
-        fn=sales_chat,
-        title="房产销售",
-        # retry_btn=None,
-        # undo_btn=None,
-        chatbot=gr.Chatbot(height=600),
-    )
+    history[-1][1] = ""
+    for character in response:
+        history[-1][1] += character
+        time.sleep(0.05)
+        yield history
 
-    demo.launch(share=True, server_name="0.0.0.0")
+
+def change_scene(choice):
+    # print(f"change_scene-[choice]{choice}")
+    vector_store_dir = choice + "_sales"
+    initialize_sales_bot(vector_store_dir)
+    return gr.Chatbot.update(value="")
+
+
+def change_enable_chat(enable):
+    global ENABLE_CHAT
+    ENABLE_CHAT = enable
+
+
+def launch_gradio_by_blocks():
+    with gr.Blocks(title="销售机器人") as blocks:
+        with gr.Row():
+            with gr.Column(scale=1):
+                with gr.Row():
+                    scene_radio = gr.Radio(
+                        [(member.name, member.value) for member in Scene_enum],
+                        label="切换场景",
+                        info="选择一个要咨询的场景",
+                        value=Scene_enum.房产,
+                    )
+                    enable_chat_checkbox = gr.Checkbox(
+                        label="激活 AI", info="通过 AI 更好的回答问题", value=ENABLE_CHAT
+                    )
+            with gr.Column(scale=4):
+                chatbot = gr.Chatbot([], elem_id="chatbot", bubble_full_width=False)
+                with gr.Row():
+                    txt = gr.Textbox(
+                        scale=4,
+                        show_label=False,
+                        placeholder=" 请输入你想咨询的问题",
+                        container=False,
+                    )
+                    # btn = gr.Button("Submit")
+        txt_msg = txt.submit(add_text, [chatbot, txt], [chatbot, txt], queue=True).then(
+            bot, chatbot, chatbot
+        )
+        txt_msg.then(lambda: gr.update(interactive=True), None, [txt], queue=False)
+
+        scene_radio.change(fn=change_scene, inputs=scene_radio, outputs=chatbot)
+
+        enable_chat_checkbox.change(fn=change_enable_chat, inputs=enable_chat_checkbox)
+
+        # btn.click(add_text, [chatbot, txt], [chatbot, txt], queue=True).then(bot, chatbot,chatbot)
+
+    blocks.queue(max_size=10)
+    blocks.launch(share=True, server_name="0.0.0.0", server_port=7861)
+
 
 if __name__ == "__main__":
     # 初始化房产销售机器人
     initialize_sales_bot()
     # 启动 Gradio 服务
-    launch_gradio()
+    launch_gradio_by_blocks()
